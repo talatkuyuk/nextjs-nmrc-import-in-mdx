afe7410e0449d7cb64f54ab5770469a016c12dc2
talatkuyuk <talatkuyuk@gmail.com>
2025-02-22 10:33:50 +0300

    updated the Navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Logo from "./Logo";
 import Navbar from "./Navbar";
 import ThemeToggle from "./ThemeToggle";
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
+  const isActive = pathname.includes("article");
+
   return (
     <div className="navbar">
-      <Link href="/articles">Articles</Link>
+      <Link href="/articles" className={isActive ? "underline" : undefined}>
+        Articles
+      </Link>
     </div>
   );
 };
