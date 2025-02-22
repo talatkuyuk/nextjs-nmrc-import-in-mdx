492557af7e4ebf0c4f2549c05d4a8a80e7dbc4a9
talatkuyuk <talatkuyuk@gmail.com>
2025-02-22 13:45:08 +0300

    added Link component into mdxComponents

--- mdxComponents/index.tsx
+++ mdxComponents/index.tsx
@@ -1,17 +1,19 @@
 import dynamic from "next/dynamic";
+import Link from "next/link";
 import Image, { type ImageProps } from "next/image";
 import type { MDXComponents } from "next-mdx-remote-client/rsc";
 
 export const components: MDXComponents = {
-  ALink: dynamic(() => import("next/link")),
   strong: (props: React.ComponentPropsWithoutRef<"strong">) => (
     <strong className="custom-strong" {...props} />
   ),
   wrapper: (props: React.ComponentPropsWithoutRef<"div">) => {
     return <div id="mdx-layout">{props.children}</div>;
   },
+  Image,
   CustomImage: function CustomImage(props: ImageProps) {
     return <Image {...props} />;
   },
-  Image,
+  DynamicLink: dynamic(() => import("next/link")),
+  Link,
 };
