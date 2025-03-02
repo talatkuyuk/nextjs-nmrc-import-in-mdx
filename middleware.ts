0d92518e3ee3e2ffa467a631826e041385ad0797
talatkuyuk <talatkuyuk@gmail.com>
2025-03-02 23:50:17 +0300

    update the middleware

--- middleware.ts
+++ middleware.ts
@@ -6,19 +6,18 @@ import { type NextRequest, NextResponse } from "next/server";
 export async function middleware(req: NextRequest): Promise<NextResponse> {
   const url = req.nextUrl.pathname;
 
-  if (url.startsWith("/blog-assets/")) {
-    const imageName = url.split("/blog-assets/")[1];
-    const imagePath = path.join(
-      process.cwd(),
-      "articles-strategy-flat-content",
-      "blog-assets",
-      imageName
-    );
+  if (url.startsWith("/data/")) {
+    const imageName = url.split("/").at(-1);
+    const imagePath = path.join(process.cwd(), url);
+
+    if (!fs.existsSync(imagePath)) {
+      return new NextResponse("File not found", { status: 404 });
+    }
 
     try {
       const data = fs.readFileSync(imagePath);
 
-      const ext = path.extname(imageName).toLowerCase();
+      const ext = path.extname(imageName!).toLowerCase();
       const contentType =
         ext === ".png"
           ? "image/png"
