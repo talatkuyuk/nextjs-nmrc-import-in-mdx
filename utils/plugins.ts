a828b3fb545f7eefc7ea9d31fb3c33d4b66ede07
talatkuyuk <talatkuyuk@gmail.com>
2025-03-06 01:24:54 +0300

    updated the plugin lists

--- utils/plugins.ts
+++ utils/plugins.ts
@@ -2,8 +2,7 @@ import type { PluggableList } from "unified";
 import { nodeTypes } from "@mdx-js/mdx";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
-import rehypeMdxImportMedia from "rehype-mdx-import-media";
-import recmaMdxEscapeMissingComponents from "recma-mdx-escape-missing-components";
+import recmaMdxImportMedia from "recma-mdx-import-media";
 import recmaMdxChangeImports, {
   type ChangeImportsOptions,
 } from "recma-mdx-change-imports";
@@ -11,15 +10,13 @@ import recmaMdxChangeImports, {
 export const remarkPlugins: PluggableList = [remarkGfm];
 
 export const rehypePlugins: PluggableList = [
-  // allow HTML elements in "md" format, "passThrough" is for "mdx"
   [rehypeRaw, { passThrough: nodeTypes }],
-  // turn media paths into imports in markdown syntax
-  rehypeMdxImportMedia,
 ];
 
-export const recmaPlugins: PluggableList = [
-  // escape if there is no "Bar" component and don't throw error
-  [recmaMdxEscapeMissingComponents, ["Bar"]],
-  // turn imports with relatie paths into constant variable declaration
-  [recmaMdxChangeImports, { pathname: "blog-assets" } as ChangeImportsOptions],
+export const recmaPlugins: (pathname: string) => PluggableList = (pathname) => [
+  // turn media paths into imports in markdown syntax
+  recmaMdxImportMedia,
+
+  // turn imports with relative paths into constant variable declaration of resolvable URL string
+  [recmaMdxChangeImports, { pathname } as ChangeImportsOptions],
 ];
