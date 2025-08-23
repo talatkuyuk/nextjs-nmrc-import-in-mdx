4e6b3f1c05901a12128e751e01dbc946ca2a9982
talatkuyuk <talatkuyuk@gmail.com>
2025-08-23 13:05:06 +0300

    Update next config (add outputFileTracingIncludes)

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    "*": ["./data/**"],
+  },
+};
 
 export default nextConfig;
